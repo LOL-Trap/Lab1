8a8c06336de4cfa3346224980a5726559a4377a4
kiratuz438 <kiratuz438@gmail.com>
2026-03-15 15:54:33 +0200

    Tests ready

---
+++ Core/Interfaces/IRefactoring.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Core.Models;
+
+namespace Core.Interfaces
+{
+    public interface IRefactoring
+    {
+        string Name { get; }
+        string Description { get; }
+        bool CanApply(string code);
+        string Apply(string code, RefactoringParameters parameters);
+    }
+}
